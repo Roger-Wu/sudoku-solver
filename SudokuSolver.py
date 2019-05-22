e98b18eb578469673c6ce6887376bc5009021569
Roger-Wu <gsetcrw@gmail.com>
2019-05-23 04:43:36 +0800

    add comments about algorithm. lint.

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,14 +1,24 @@
 """
 written in python 3
 
-goals:
-1. Never copy the board when solving.
-2. Same solving process even when we need to "guess".
+algorithm: backtracking (dfs) with an optimal filling order
+1. Build a list (or a set) for each cell to store the possible answers of the cell
+2. Find the cell with least possible answers. Fill the cell with one of the possible answers. Update the possible answers of other cells.
+3. Repeat step 2 until solved. Or if there's some cells without possible answers, undo and try the next possible answer.
 """
 
+
 class SudokuSolver(object):
+    col_size = 9  # len(self.board)
+    row_size = 9  # len(self.board[0])
+    block_col_size = 3
+    block_row_size = 3
+
+    digit_unfilled = 0
+    digit_min = 1
+    digit_max = 9
+
     def solveSudoku(self, board):
-        self.init_params()
         self.read_board(board)
         self.init_solving_utils()
 
@@ -19,19 +29,11 @@ class SudokuSolver(object):
 
         # return self.board
 
-    def init_params(self):
-        self.col_size = 9  # len(self.board)
-        self.row_size = 9  # len(self.board[0])
-        self.block_col_size = 3
-        self.block_row_size = 3
-
-        self.digit_unfilled = 0
-        self.digit_min = 1
-        self.digit_max = 9
-
     def read_board(self, charBoard):
-        self.initial_board = [[self.char_to_int(char) for char in row] for row in charBoard]
-        self.board = [[self.digit_unfilled for char in row] for row in charBoard]
+        self.initial_board = [
+            [self.char_to_int(char) for char in row] for row in charBoard]
+        self.board = [[self.digit_unfilled for char in row]
+                      for row in charBoard]
 
     def char_to_int(self, char):
         if char == '.':
@@ -44,7 +46,8 @@ class SudokuSolver(object):
             print(row)
 
     def init_solving_utils(self):
-        self.psbl_digits = [[set(range(self.digit_min, self.digit_max + 1)) for ci in range(self.col_size)] for ri in range(self.row_size)]
+        self.psbl_digits = [[set(range(self.digit_min, self.digit_max + 1))
+                             for ci in range(self.col_size)] for ri in range(self.row_size)]
         self.unfilled_cells = []
         # In order to unfill cells
         self.each_step_updated_cells = []
@@ -110,7 +113,8 @@ class SudokuSolver(object):
             return True
 
         # get the cell with fewest possible answers
-        self.unfilled_cells.sort(reverse=True, key=lambda cell: len(self.psbl_digits[cell[0]][cell[1]]))
+        self.unfilled_cells.sort(reverse=True, key=lambda cell: len(
+            self.psbl_digits[cell[0]][cell[1]]))
         cell = self.unfilled_cells.pop()
         row, col = cell
 
@@ -138,4 +142,14 @@ class SudokuSolver(object):
 
 if __name__ == '__main__':
     sol = SudokuSolver()
-    sol.solveSudoku(["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."])
+    sol.solveSudoku([
+        "..9748...",
+        "7........",
+        ".2.1.9...",
+        "..7...24.",
+        ".64.1.59.",
+        ".98...3..",
+        "...8.3.2.",
+        "........6",
+        "...2759.."
+    ])
